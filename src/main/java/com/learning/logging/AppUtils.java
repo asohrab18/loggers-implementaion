60048f754456aa6baf0569a7821a20be9c8a18e8
Sohrab Alam <sohrab.alam86@gmail.com>
2025-12-11 17:22:53 +0530

    Updated AppUtils and log4j.properties; added proper .gitignore

--- src/main/java/com/learning/logging/AppUtils.java
+++ src/main/java/com/learning/logging/AppUtils.java
@@ -7,7 +7,7 @@ public class AppUtils {
 	private static final Logger LOG = Logger.getLogger(AppUtils.class);
 
 	public static void show() {
-		LOG.info("Hello World!");
+		LOG.info("Hello World of Loggers Impl!");
 		LOG.info("Welcome Back!");
 	}
 
